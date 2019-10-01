ba104fdac685496f72a9a97a5d0220745021b964
QualityHammer <45611321+qualityhammer@users.noreply.github.com>
2019-09-30 20:51:15 -0400

    Created testing code

--- instrumental_dl/__init__.py
+++ instrumental_dl/__init__.py
@@ -1,8 +1,37 @@
 import sys
+import youtube_dl
+
+
+class Logger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
 
 
 def main():
-    print('args:', sys.argv)
+    options = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'logger': Logger(),
+        'progress_hooks': [hook],
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download(['https://www.youtube.com/watch?v=4N1bIfAlBPc'])
 
 
 __all__ = ['main']
--- instrumental_dl/run.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-
-def main():
-    print('args:', sys.argv)
