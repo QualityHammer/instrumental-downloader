7285423de383cdebb885adcc9492ff567ea8e10f
QualityHammer <45611321+qualityhammer@users.noreply.github.com>
2019-10-02 17:14:27 -0400

    Made the timer actually accurate

--- instrumental_dl/logger/logger.py
+++ instrumental_dl/logger/logger.py
@@ -4,23 +4,28 @@ class Logger:
         if song_titles is None:
             song_titles = []
         self.elapsed = 0
+        self.download_elapsed = 0
+        self.conversion_elapsed = 0
         self.song_count = 0
         self.file_names = []
         self.song_titles = song_titles.copy()
 
     def log_append(self, filename: str, elapsed: float):
         self.song_count += 1
-        self.elapsed += elapsed
+        self.download_elapsed += elapsed
         self.song_titles.append(filename[:-4])
         self.file_names.append(filename)
-        # print(filename, 'finished downloading in', round(elapsed, 4), 'seconds')
 
     def hook(self, download):
         if download['status'] == 'finished':
             self.log_append(download['filename'], download['elapsed'])
 
-    def print_log(self):
-        print('Downloading', self.song_count, 'songs took', self.elapsed, 'seconds.')
+    def print_log(self, elapsed):
+        self.elapsed = elapsed
+        self.conversion_elapsed = elapsed - self.download_elapsed
+        print('Downloading', self.song_count, 'songs took', self.download_elapsed,
+              'seconds.\nConversion took', self.conversion_elapsed,
+              'seconds, and full process took', self.elapsed, 'seconds.')
         with open('../log/download_list.txt', 'w+') as file:
             for i in range(len(self.file_names)):
                 file.write(self.song_titles[i] + ' as: ' + self.file_names[i] + '\n')
--- instrumental_dl/run.py
+++ instrumental_dl/run.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from .youtube_dl_wrapper.wrapper import YoutubeDL
 from .common.file_reader import read_txt_file
@@ -7,13 +8,17 @@ from .logger.logger import Logger
 
 def real_main():
     if len(sys.argv) > 1:
+        start_time = time.time()
         if sys.argv[1][-4:] == '.txt':
+            print("Downloading and converting songs from " + sys.argv[1] + "...")
             song_names = read_txt_file(sys.argv[1])
         else:
+            print("Downloading and converting songs...")
             song_names = sys.argv
             song_names.pop(0)
         logger = Logger(song_names)
         YoutubeDL(logger).download_songs(song_names)
-        logger.print_log()
+        logger.print_log(time.time() - start_time)
     else:
-        print("Error: enter a song name to download")
+        print("Error: enter either a .txt filename or "
+              "song names to download")
