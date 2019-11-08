bb116a7df0df0f5a2605d75a6859ef39b47c2af3
QualityHammer <45611321+qualityhammer@users.noreply.github.com>
2019-11-08 15:20:50 -0500

    Separated all messages into a new class for organization

--- instrumental_dl/logger/logger.py
+++ instrumental_dl/logger/logger.py
@@ -7,11 +7,67 @@ from ..common.args import is_verbose
 
 
 class ConstMessages:
+    """
+    ConstMessages fields and methods are used to separate the display messages from the logic.
+    """
+
     long_file_separator = '---------------------------------------------------\n'
     no_instrumental = "Instrumentals for these songs could not be found:"
     no_instrumental_basic = "Instrumentals for some of the songs on the list could " \
                             "not be found.\nSee download_list.txt in " \
                             "~user/music/Instrumentals/instrumental_dl_log"
+
+    @staticmethod
+    def finished_message(song_count: int, elapsed: str):
+        """
+        Returns the normal message that is printed on completion of all downloads, conversions, and renaming.
+
+        :param song_count: The amount of songs that finished
+        :param elapsed: The full time that elapsed during the process
+        :return: The final normal message
+        """
+        s = ConstMessages._get_s_char(song_count)
+        return f'Downloaded {song_count} song{s} in {elapsed} seconds.'
+
+    @staticmethod
+    def log_song_download(song_name: str, filename: str):
+        return f'{song_name} as: {filename} \n'
+
+    @staticmethod
+    def log_failed_song_download(song: str):
+        return f"{song}'s instrumental could not be found"
+
+    @staticmethod
+    def verbose_finished_message(song_count: int, download_elapsed: str, conversion_elapsed: str, elapsed: str):
+        """
+        Returns the verbose message that is printed on completion of all downloads, conversions, and renaming.
+
+        :param song_count: The amount of songs that finished
+        :param download_elapsed: The time that elapsed during the download process
+        :param conversion_elapsed: The time that elapsed during the conversion and renaming process
+        :param elapsed: The full time that elapsed during the process
+        :return: The final verbose message
+        """
+        s = ConstMessages._get_s_char(song_count)
+        return f'Downloading {song_count} song{s} took {download_elapsed} ' \
+               f'seconds.\nConversion took {conversion_elapsed} ' \
+               f'seconds, and full process took {elapsed} seconds.'
+
+    @staticmethod
+    def verbose_song_downloaded_message(filename: str):
+        return f'Downloaded {filename}.'
+
+    @staticmethod
+    def _get_s_char(song_count: int):
+        """
+        Return's an 's' for pluralization if the amount of songs downloaded is greater than 1
+
+        :param song_count: The amount of songs that finished
+        :return: Either a blank character '' or 's'
+        """
+        if song_count == 1:
+            return ''
+        return 's'
 
 
 class Logger:
@@ -63,8 +119,9 @@ class Logger:
                 self.log_append(download['filename'], download['elapsed'])
             except KeyError:
                 self.log_append(download['filename'], 0)
+
             if is_verbose():
-                print(f'Downloaded {download["filename"]}.')
+                print(ConstMessages.verbose_song_downloaded_message(download["filename"]))
 
     def print_log(self, elapsed: float):
         """Prints the final log when all of the instrumentals finish"""
@@ -113,22 +170,18 @@ class Logger:
         # Console message only displays if more than one instrumental was successfully downloaded
         print('\n')
         if self.song_count != 0:
-            if self.song_count == 1:
-                s = ''
-            else:
-                s = 's'
             if is_verbose():
                 # Verbose message
-                print(f'Downloading {self.song_count} song{s} took {self.download_elapsed} '
-                      f'seconds.\nConversion took {self.conversion_elapsed} '
-                      f'seconds, and full process took {self.elapsed} seconds.')
+                print(ConstMessages.verbose_finished_message(self.song_count, str(self.conversion_elapsed),
+                                                             str(self.download_elapsed), str(self.elapsed)))
             else:
-                print(f'Downloaded {self.song_count} song{s} in {self.elapsed} seconds.')
+                print(ConstMessages.finished_message(self.song_count, str(self.elapsed)))
         # Failed instrumental message
         if len(self.failed_songs) > 0:
             if is_verbose():
                 # Verbose message
                 print(ConstMessages.no_instrumental)
+                # Prints out each song that failed
                 for song in self.failed_songs:
                     print(song)
             else:
@@ -140,9 +193,9 @@ class Logger:
         with open(f'{self._create_log_dir()}/download_list.txt', 'w+') as file:
             # Each instrumental downloaded
             for i in range(len(self.file_names)):
-                file.write(f'{self.song_titles[i]} as: {self.file_names[i]} \n')
+                file.write(ConstMessages.log_song_download(self.song_titles[i], self.file_names[i]))
             # Any instrumentals that couldn't be found
             if len(self.failed_songs) > 0:
                 file.write(ConstMessages.long_file_separator)
                 for song in self.failed_songs:
-                    file.write(f"{song}'s instrumental could not be found")
+                    file.write(ConstMessages.log_failed_song_download(song))
