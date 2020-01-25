89fe46499a38df9e84e57fda95bdfe5dda4a360d
QualityHammer <agingllama@gmail.com>
2020-01-24 19:21:40 -0500

    Added log messages
    Signed-off-by: Ryan Cohen <ryan@cortexcollaborators.com>

--- instrumental_dl/args.py
+++ instrumental_dl/args.py
@@ -1,13 +1,26 @@
 import argparse
 from json import loads
+from logging import getLogger
 from os.path import dirname, realpath, join, isfile
+from sys import exit
 
 from instrumental_dl import __version__
+
+USAGE_MSG = "Usage: instrumental_dl -f FILE_NAME\n" \
+            "   instrumental_dl -s 'SONG_NAMES' ['SONG_NAMES'...]\n" \
+            "       (with -s the song name needs to be in quotations\n" \
+            "        if it contains any spaces)"
+HELP_NOT_FOUND = "The help file could not be found.\n" \
+                 "      This should not occur, so contact the developer with this issue " \
+                 "if possible."
 
 
 def get_arguments():
     long_args = {'-f': "--file", '-s': "--songs", '-v': "--verbose", '-o': "--output"}
     arg_help = _get_argument_help()
+    if not arg_help:
+        # This will only occur if the help file is not found for whatever reason
+        arg_help = {key: "" for key in long_args.keys()}
     parser = argparse.ArgumentParser(prog='instrumental_dl', description='A downloader for instrumentals.')
     parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}")
     parser.add_argument("-f", long_args["-f"], metavar="FILE", nargs='?', required=False, help=arg_help["-f"])
@@ -17,18 +30,18 @@ def get_arguments():
 
     args = parser.parse_args()
     if not hasattr(args, 'f') and not hasattr(args, 's'):
-        # TODO: Add usage message
-        print("Usage: ")
+        print('\n', USAGE_MSG)
+        exit(1)
 
     return args
 
 
 def _get_argument_help():
+    logger = getLogger("arguments")
     filename = join(dirname(dirname(realpath(__file__))), "config", "arg_help.json")
     if isfile(filename):
         with open(filename, "r") as arg_help:
             return loads(arg_help.read())
     else:
-        # TODO: Add config exception
-        print("Error: config file not found")
+        logger.error(HELP_NOT_FOUND)
         return None
